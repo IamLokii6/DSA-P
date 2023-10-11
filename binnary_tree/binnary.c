e218c2d3700140a9ee699c8492b20ada55b53e76
IamLokii6 <painginkarlokesh@gmail.com>
2023-10-11 19:53:20 +0530

    binnary tree

---
+++ binnary_tree/binnary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "binary_tree.h"
+
+void preorderTraversal(struct node *root)
+{
+    if (root == NULL)
+        return;
+    printf("%d ->", root->item);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
---
+++ binnary_tree/binnary.h
@@ -0,0 +1,11 @@
+#ifndef BINARY_TREE
+#define BINARY_TREE
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+#endif
